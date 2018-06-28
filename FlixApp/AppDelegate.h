b0aa4251476c0b3f29c7fc8c934ad6044faf4732
Nihal Riyadh Jemal <nihalj@fb.com>
2018-06-27 17:19:11 -0700

    Initial Commit

---
+++ FlixApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FlixApp
+//
+//  Created by Nihal Riyadh Jemal on 6/27/18.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FlixApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FlixApp
+//
+//  Created by Nihal Riyadh Jemal on 6/27/18.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
