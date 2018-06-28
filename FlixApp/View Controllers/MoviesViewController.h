812405456056de40b112454b045ddcc276b49c62
Nihal Riyadh Jemal <nihalj@fb.com>
2018-06-27 22:10:28 -0700

    Commit3

--- FlixApp/View Controllers/MoviesViewController.h
+++ FlixApp/View Controllers/MoviesViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  MoviesViewController.h
 //  FlixApp
 //
 //  Created by Nihal Riyadh Jemal on 6/27/18.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface MoviesViewController : UIViewController
 
 @end
-
